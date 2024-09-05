adb12d2f12b414e5ae5faf82abfdf471523ac222
jhonsux <jhoonsux@gmail.com>
2024-09-05 10:56:20 -0600

    cambios

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
 // config.js
-const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_HOST = process.env.DB_HOST || 'mysql.railway.internal';
 const DB_USER = process.env.DB_USER || 'root';
-const DB_PASSWORD = process.env.DB_PASSWORD || 'jhon1';
-const DB_NAME = process.env.DB_NAME || 'centro_escolar';
+const DB_PASSWORD = process.env.DB_PASSWORD || 'PAInjSAPICBzQgESmwngCDPiXJWkWKeE';
+const DB_NAME = process.env.DB_NAME || 'railway';
+const DB_PORT = process.env.DB_PORT || 3306;
 
 module.exports = {
     DB_HOST,
     DB_USER,
     DB_PASSWORD,
-    DB_NAME
+    DB_NAME,
+    DB_PORT
 };
--- db.js
+++ db.js
@@ -1,14 +1,15 @@
 
 
 // db.js
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = require('./config');
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = require('./config');
 
 const mysql = require('mysql2')
 const conexion = mysql.createConnection({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
-    database: DB_NAME
+    database: DB_NAME,
+    port: DB_PORT
 });
 
 conexion.connect((err) => {
