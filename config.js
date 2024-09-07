997c7c692b9761ccb790a8ba48634fe784ca8915
jhonsux <jhoonsux@gmail.com>
2024-09-06 18:42:51 -0600

    cambios en db

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
-
+//mysql://root:NqUZGHYxcQhseahmOKCFTuhfpKgVGSHr@autorack.proxy.rlwy.net:51150/railway
 // config.js
-const MYSQLHOST = process.env.MYSQLHOST || 'localhost';
+const MYSQLHOST = process.env.DB_HOST || 'localhost';
 const DB_USER = process.env.DB_USER || 'root';
 const DB_PASSWORD = process.env.DB_PASSWORD || 'jhon';
 const DB_NAME = process.env.DB_NAME || 'Centro_escolar';
 const DB_PORT = process.env.DB_PORT || 3306;
 
 module.exports = {
-    MYSQLHOST,
+    DB_HOST,
     DB_USER,
     DB_PASSWORD,
     DB_NAME,
     DB_PORT
 };
 
-console.log('DB_HOST:', MYSQLHOST);
+console.log('DB_HOST:', DB_HOST);
 console.log('DB_USER:', DB_USER);
 console.log('DB_PASSWORD:', DB_PASSWORD);
 console.log('DB_NAME:', DB_NAME);
--- db.js
+++ db.js
@@ -1,9 +1,9 @@
-const { MYSQLHOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = require('./config');
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = require('./config');
 const mysql = require('mysql2');
 
 // Crear un pool de conexiones
 const pool = mysql.createPool({
-    host: MYSQLHOST,
+    host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
     database: DB_NAME,
